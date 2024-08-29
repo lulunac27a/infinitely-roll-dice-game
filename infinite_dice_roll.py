78128f986ea08bbdeab023bdb327e38c260223d7
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-08-29 18:07:44 -0500

    Format code

--- infinite_dice_roll.py
+++ infinite_dice_roll.py
@@ -1,24 +1,26 @@
 """
 Dice game that rolls infinitely number of times until last rolled dice is not 6.
 """
-import random #generate random numbers
+
+import random  # generate random numbers
+
 
 def roll_dice():
     """
     Rolls the dice.
     """
-    return random.randint(1, 6) #roll 6-sized dice
-    
+    return random.randint(1, 6)  # roll 6-sized dice
+
+
 def roll_and_multiply():
     """
     Rolls the dice infinite number of times until last rolled dice is not 6, then multiply the rolled dice.
     """
-    result = 1 #set result to 1
-    roll = roll_dice()# roll the dice
-    while roll == 6: #while rolled dice is 6
-        result *= 6 #multiply result by 6
-        roll = roll_dice() #roll the dice again
+    result = 1  # set result to 1
+    roll = roll_dice()  # roll the dice
+    while roll == 6:  # while rolled dice is 6
+        result *= 6  # multiply result by 6
+        roll = roll_dice()  # roll the dice again
 
-    result *= roll #multiply result by last rolled dice
-    return result #return the result
-    
+    result *= roll  # multiply result by last rolled dice
+    return result  # return the result
--- infinite_dice_roll_with_sides.py
+++ infinite_dice_roll_with_sides.py
@@ -1,24 +1,26 @@
 """
 Dice game that rolls infinitely number of times until last rolled dice is not the number of sides of the dice.
 """
-import random #generate random numbers
+
+import random  # generate random numbers
+
 
 def roll_dice(sides):
     """
     Rolls the dice with specified number of sides
     """
-    return random.randint(1, sides) #roll the dice with specified number of sides
-    
+    return random.randint(1, sides)  # roll the dice with specified number of sides
+
+
 def roll_and_multiply(sides):
     """
     Rolls the dice infinite number of times until last rolled dice is not the number of sides of the dice, then multiply the rolled dice.
     """
-    result = 1 #set result to 1
-    roll = roll_dice(sides) #roll the dice with specified number of sides of dice
-    while roll == sides: #while rolled dies is not the number of sides of dice
-        result *= sides #multiply result by number of sides of dice
-        roll = roll_dice(sides) #roll the dice again
+    result = 1  # set result to 1
+    roll = roll_dice(sides)  # roll the dice with specified number of sides of dice
+    while roll == sides:  # while rolled dies is not the number of sides of dice
+        result *= sides  # multiply result by number of sides of dice
+        roll = roll_dice(sides)  # roll the dice again
 
-    result *= roll #multiply result by last rolled dice
-    return result #return the result
-    
+    result *= roll  # multiply result by last rolled dice
+    return result  # return the result
