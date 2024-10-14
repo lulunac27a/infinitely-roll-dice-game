9ed67b00dfb7ff7a80de0d9904e8cbd6f3ccf1f3
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-10-14 15:03:11 -0500

    Add parameter descriptions in docstrings

--- infinite_dice_roll_with_sides.py
+++ infinite_dice_roll_with_sides.py
@@ -7,7 +7,8 @@ import random  # generate random numbers
 
 def roll_dice(sides):
     """
-    Rolls the dice with specified number of sides
+    Rolls the dice with specified number of sides.
+    sides - the number of sides to roll.
     """
     return random.randint(1, sides)  # roll the dice with specified number of sides
 
@@ -15,6 +16,7 @@ def roll_dice(sides):
 def roll_and_multiply(sides):
     """
     Rolls the dice infinite number of times until last rolled dice is not the number of sides of the dice, then multiply the rolled dice.
+    sides - the number of sides to roll.
     """
     result = 1  # set result to 1
     roll = roll_dice(sides)  # roll the dice with specified number of sides of dice
